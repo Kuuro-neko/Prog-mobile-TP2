7e5fb67420fbed1b05901f2e349cabee165845f5
Gilles Gonzalez Oropeza <gilles.gonzalezoropeza@gmail.com>
2025-02-18 14:35:28 +0100

    GPS qui affiche en tetxe longitude latitude

--- app/src/main/java/com/example/prog_mobile_tp2/GeolocalizationActivity.java
+++ app/src/main/java/com/example/prog_mobile_tp2/GeolocalizationActivity.java
@@ -1,24 +1,79 @@
 package com.example.prog_mobile_tp2;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.location.CurrentLocationRequest;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
+import com.google.android.gms.tasks.CancellationToken;
+import com.google.android.gms.tasks.Task;
+
 public class GeolocalizationActivity extends MenuActivity {
+    private FusedLocationProviderClient fusedLocationClient;
+    private TextView position;
+    private Button refresh;
+    private CurrentLocationRequest locationRequest;
+    private CancellationToken cancellationToken;
+    private Task<Location> location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_geolocalization);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        position = findViewById(R.id.position_output);
+        refresh = findViewById(R.id.refresh_button);
+
+
+        displayLocation();
+
+        refresh.setOnClickListener(v -> {
+            displayLocation();
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+    }
+
+    private void displayLocation() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details
+
+            // do the call to request the permissions
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
+
+            return;
+        }
+        location = fusedLocationClient.getCurrentLocation(Priority.PRIORITY_BALANCED_POWER_ACCURACY, cancellationToken);
+        location.addOnSuccessListener(this, location -> {
+            if (location != null) {
+                position.setText(getText(R.string.latitude).toString() +location.getLatitude() + "\n" + getText(R.string.longitude) + location.getLongitude());
+            } else {
+                position.setText(getText(R.string.no_location));
+            }
         });
     }
 }

d9ca4cebbc5a59a14445c72c783603e5d3a49c5b
Gilles Gonzalez Oropeza <gilles.gonzalezoropeza@gmail.com>
2025-02-18 15:58:07 +0100

    try read country data, refactor gps

--- app/src/main/java/com/example/prog_mobile_tp2/CountrySelectActivity.java
+++ app/src/main/java/com/example/prog_mobile_tp2/CountrySelectActivity.java
@@ -1,24 +1,115 @@
 package com.example.prog_mobile_tp2;
 
+import android.content.res.XmlResourceParser;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 
 public class CountrySelectActivity extends MenuActivity {
+    private RecyclerView recyclerView;
+
+    public class Country {
+        public String name;
+        public String capital;
+        public int area;
+        public int population;
+        public String currency;
+        public String language;
+        public String flag;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_country_select);
+
+        recyclerView = findViewById(R.id.country_list);
+
+        ArrayList<Country> countries = readData();
+
+        String[] countryNames = new String[countries.size()];
+        for (int i = 0; i < countries.size(); i++) {
+            countryNames[i] = countries.get(i).name;
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, countryNames);
+
+       // recyclerView.setAdapter(adapter);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private ArrayList<Country> readData() {
+        ArrayList<Country> countries = new ArrayList<>();
+        try {
+            XmlResourceParser parser = getResources().getXml(R.xml.countries);
+            int eventType = parser.getEventType();
+            Country country = null;
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                String tagName = parser.getName();
+                switch (eventType) {
+                    case XmlPullParser.START_TAG:
+                        if ("country".equals(tagName)) {
+                            country = new Country();
+                        } else if (country != null) {
+                            switch (tagName) {
+                                case "name":
+                                    country.name = parser.nextText();
+                                    break;
+                                case "capital":
+                                    country.capital = parser.nextText();
+                                    break;
+                                case "area":
+                                    country.area = Integer.parseInt(parser.nextText());
+                                    break;
+                                case "population":
+                                    country.population = Integer.parseInt(parser.nextText());
+                                    break;
+                                case "currency":
+                                    country.currency = parser.nextText();
+                                    break;
+                                case "language":
+                                    country.language = parser.nextText();
+                                    break;
+                                case "flag":
+                                    country.flag = parser.nextText();
+                                    break;
+                            }
+                        }
+                        break;
+                    case XmlPullParser.END_TAG:
+                        if ("country".equals(tagName) && country != null) {
+                            countries.add(country);
+                        }
+                        break;
+                }
+                eventType = parser.next();
+            }
+        } catch (XmlPullParserException | IOException e) {
+            e.printStackTrace();
+        }
+        for (Country country : countries) {
+            System.out.println(country.name);
+        }
+        return countries;
     }
 }
--- app/src/main/java/com/example/prog_mobile_tp2/GeolocalizationActivity.java
+++ app/src/main/java/com/example/prog_mobile_tp2/GeolocalizationActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
@@ -18,6 +19,7 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.Priority;
 import com.google.android.gms.tasks.CancellationToken;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
 public class GeolocalizationActivity extends MenuActivity {
@@ -53,27 +55,21 @@ public class GeolocalizationActivity extends MenuActivity {
     }
 
     private void displayLocation() {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details
-
-            // do the call to request the permissions
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
-
-            return;
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+            fusedLocationClient.getLastLocation()
+                    .addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Location> task) {
+                            if (task.isSuccessful() && task.getResult() != null) {
+                                Location location = task.getResult();
+                                position.setText(getString(R.string.latitude) + location.getLatitude() + "\n" + getString(R.string.longitude) + location.getLongitude());
+                            } else {
+                                position.setText(R.string.no_location);
+                            }
+                        }
+                    });
         }
-        location = fusedLocationClient.getCurrentLocation(Priority.PRIORITY_BALANCED_POWER_ACCURACY, cancellationToken);
-        location.addOnSuccessListener(this, location -> {
-            if (location != null) {
-                position.setText(getText(R.string.latitude).toString() +location.getLatitude() + "\n" + getText(R.string.longitude) + location.getLongitude());
-            } else {
-                position.setText(getText(R.string.no_location));
-            }
-        });
     }
 }
